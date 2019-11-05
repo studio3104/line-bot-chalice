4b37c421a9db5b8e3e8141608a755ba8bc51a6d3
studio3104 <studio3104.com@gmail.com>
2019-11-05 17:12:15 +0900

    1st implementation commit

---
+++ chalicelib/__init__.py
@@ -0,0 +1,26 @@
+import boto3
+
+import os
+
+
+sqs = boto3.resource('sqs')
+sqs_queue_name = os.environ['SQS_QUEUE_NAME']
+
+rekognition_client = boto3.client('rekognition')
+
+
+class __ObjectManager:
+    _sqs_queue = None
+
+    @property
+    def sqs_queue(self):
+        if not self._sqs_queue:
+            self._sqs_queue = sqs.get_queue_by_name(QueueName=sqs_queue_name)
+        return self._sqs_queue
+
+
+__om = __ObjectManager()
+
+
+def get_sqs_queue():
+    return __om.sqs_queue
---
+++ tests/test_app.py
@@ -0,0 +1,56 @@
+import pytest
+
+from http import HTTPStatus
+import base64
+import os
+import hmac
+import hashlib
+import json
+
+
+class TestCallback:
+    @pytest.fixture
+    def url(self) -> str:
+        return '/callback'
+
+    def test_post_200(self, url: str, client) -> None:
+        request_json = json.dumps({
+            'events': [{
+                'type': 'message',
+                'replyToken': '7c8a3dcb6eb94eb790a2d3bb38f12f93',
+                'source': {
+                    'userId': '',
+                    'type': 'user'
+                },
+                'timestamp': 1571754325743,
+                'message': {
+                    'type': 'text',
+                    'id': '10786671259466',
+                    'text': 'Hello'
+                }
+            }],
+            'destination': ''
+        })
+        signature = base64.b64encode(hmac.new(
+            os.getenv('LINE_CHANNEL_SECRET').encode('utf-8'),
+            request_json.encode('utf-8'),
+            hashlib.sha256,
+        ).digest()).decode('utf-8')
+
+        headers = {'X-Line-Signature': signature, 'Content-Type': 'application/json'}
+        response = client.post(url, headers=headers, body=request_json)
+
+        assert response.status_code == HTTPStatus.OK
+
+    def test_post_400(self, url: str, client) -> None:
+        headers = {'X-Line-Signature': ''}
+        response = client.post(url, headers=headers)
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
+    @pytest.mark.parametrize(
+        'method',
+        ('get', 'head', 'put', 'delete', 'trace', 'patch', 'link', 'unlink'),
+    )
+    def test_405(self, method: str, url: str, client) -> None:
+        response = getattr(client, method)(url)
+        assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
